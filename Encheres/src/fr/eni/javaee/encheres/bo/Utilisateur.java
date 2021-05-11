8ccd1e31a2e3122eff90550994b1463ed37b83cd
morfouesse <antoine.morganmorfouesse@gmail.com>
2021-05-11 10:44:43 +0200

    test librairie

--- Encheres/src/fr/eni/javaee/encheres/bo/Utilisateur.java
+++ Encheres/src/fr/eni/javaee/encheres/bo/Utilisateur.java
@@ -1,12 +1,25 @@
 /**
- * 
+ *
  */
 package fr.eni.javaee.encheres.bo;
+
+import java.util.Date;
 
 /**
  * @author amorf
  *
  */
 public class Utilisateur {
+	private int noUtilisateur;
+	private String pseudo;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String telephone;
+	private String rue;
+	private String codePostal;
+	private String ville;
+	private String motDePasse;
+	private Date date;
 
 }
