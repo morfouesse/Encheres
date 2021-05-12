c991a1d6e81277efafccf15a39844df959c4f2c3
morfouesse <antoine.morganmorfouesse@gmail.com>
2021-05-12 17:30:22 +0200

    constructeur modif

--- Encheres/src/fr/eni/javaee/encheres/bo/Utilisateur.java
+++ Encheres/src/fr/eni/javaee/encheres/bo/Utilisateur.java
@@ -40,26 +40,6 @@ public class Utilisateur {
 		this.noUtilisateur = noUtilisateur;
 	}
 
-	public Utilisateur(String pseudo, String nom, String prenom, String email, String telephone, String rue,
-			String codePostal, String ville, String motDePasse, int credit, boolean administrateur,
-			List<ArticleVendu> articleVenduLst, List<Enchere> enchereLst) {
-		this();
-		this.pseudo = pseudo;
-		this.nom = nom;
-		this.prenom = prenom;
-		this.email = email;
-		this.telephone = telephone;
-		this.rue = rue;
-		this.codePostal = codePostal;
-		this.ville = ville;
-		this.motDePasse = motDePasse;
-		this.credit = credit;
-		this.administrateur = administrateur;
-		this.articleVenduLst = articleVenduLst;
-		this.enchereLst = enchereLst;
-	}
-
-
 	public Utilisateur(int noUtilisateur, String pseudo, String nom, String prenom, String email, String telephone,
 			String rue, String codePostal, String ville, String motDePasse, int credit, boolean administrateur) {
 		this(noUtilisateur);
@@ -75,8 +55,6 @@ public class Utilisateur {
 		this.credit = credit;
 		this.administrateur = administrateur;
 	}
-
-
 
 	public Utilisateur(int noUtilisateur, String pseudo, String nom, String prenom, String email, String telephone,
 			String rue, String codePostal, String ville, String motDePasse, int credit, boolean administrateur,
