cfeeb6920216efd867216fda271a8e9a516f2ba0
morfouesse <antoine.morganmorfouesse@gmail.com>
2021-05-14 13:47:50 +0200

    constructeur utilisateur

--- Encheres/src/fr/eni/javaee/encheres/bo/Utilisateur.java
+++ Encheres/src/fr/eni/javaee/encheres/bo/Utilisateur.java
@@ -56,6 +56,23 @@ public class Utilisateur {
 		this.administrateur = administrateur;
 	}
 
+	public Utilisateur(String pseudo, String nom, String prenom, String email, String telephone,
+			String rue, String codePostal, String ville, String motDePasse, int credit, boolean administrateur) {
+		this();
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.motDePasse = motDePasse;
+		this.credit = credit;
+		this.administrateur = administrateur;
+	}
+
+
 	public Utilisateur(int noUtilisateur, String pseudo, String nom, String prenom, String email, String telephone,
 			String rue, String codePostal, String ville, String motDePasse, int credit, boolean administrateur,
 			List<ArticleVendu> articleVenduLst, List<Enchere> enchereLst) {
