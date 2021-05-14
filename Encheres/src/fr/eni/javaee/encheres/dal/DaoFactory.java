7d7b2bc08e1f508bfba0427b39886a4540aeeccc
mflechard2021 <mflechard2021@50SE7-2Y9QT72.ad.campus-eni.fr>
2021-05-14 09:44:06 +0200

    Correction classe DaoFactory

--- Encheres/src/fr/eni/javaee/encheres/dal/DaoFactory.java
+++ Encheres/src/fr/eni/javaee/encheres/dal/DaoFactory.java
@@ -2,6 +2,8 @@ package fr.eni.javaee.encheres.dal;
 
 import fr.eni.javaee.encheres.dal.articleVendu.ArticleVenduDao;
 import fr.eni.javaee.encheres.dal.articleVendu.ArticleVenduDaoJdbcImpl;
+import fr.eni.javaee.encheres.dal.utilisateur.UtilisateurDao;
+import fr.eni.javaee.encheres.dal.utilisateur.UtilisateurDaoJdbcImpl;
 
 public class DaoFactory {
 
@@ -24,8 +26,12 @@ public class DaoFactory {
 	 */
 	
 	//on peut enlever le static si on utilise le singleton
-	public static ArticleVenduDao getRepasDao() {
+	public static ArticleVenduDao getArticleVenduDao() {
 		return new ArticleVenduDaoJdbcImpl();
+	}
+	
+	public static UtilisateurDao getUtilisateurDao() {
+		return new UtilisateurDaoJdbcImpl();
 	}
 	
 }
