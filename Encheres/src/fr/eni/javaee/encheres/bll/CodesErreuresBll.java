4e6f025f453afbfc2401df0316b91f07e14d8cd0
morfouesse <antoine.morganmorfouesse@gmail.com>
2021-05-18 17:33:03 +0200

    gestions des erreurs bll

---
+++ Encheres/src/fr/eni/javaee/encheres/bll/CodesErreuresBll.java
@@ -0,0 +1,30 @@
+/**
+ *
+ */
+package fr.eni.javaee.encheres.bll;
+
+/**
+ * @author amorf
+ *
+ */
+public class CodesErreuresBll {
+
+
+	public static final int PSEUDO_NULL_ERREUR = 20_000;
+	public static final int PSEUDO_ISBLANK_ERREUR = 20_001;
+	public static final int NOM_NULL_ERREUR = 20_002;
+	public static final int NOM_ISBLANK_ERREUR = 20_003;
+	public static final int PRENOM_NULL_ERREUR = 20_004;
+	public static final int PRENOM_ISBLANK_ERREUR = 20_005;
+	public static final int EMAIL_NULL_ERREUR = 20_006;
+	public static final int EMAIL_ISBLANK_ERREUR = 20_007;
+	public static final int RUE_NULL_ERREUR = 20_008;
+	public static final int RUE_ISBLANK_ERREUR = 20_009;
+	public static final int CODEPOSTAL_NULL_ERREUR = 20_010;
+	public static final int CODEPOSTAL_ISBLANK_ERREUR = 20_011;
+	public static final int VILLE_NULL_ERREUR = 20_012;
+	public static final int VILLE_ISBLANK_ERREUR = 20_013;
+	public static final int MOTDEPASSE_NULL_ERREUR = 20_014;
+	public static final int MOTDEPASSE_ISBLANK_ERREUR = 20_015;
+
+}
