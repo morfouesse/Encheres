9c206879972715f2cfef051797a70a672fbb2a48
morfouesse <antoine.morganmorfouesse@gmail.com>
2021-05-18 09:15:25 +0200

    debut de servlet coté ihm

--- Encheres/src/fr/eni/javaee/encheres/ihm/ServletAccueil.java
+++ Encheres/src/fr/eni/javaee/encheres/ihm/ServletAccueil.java
@@ -12,20 +12,20 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ServletAccueil")
 public class ServletAccueil extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-   
+
+
     public ServletAccueil() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 		this.getServletContext().getRequestDispatcher("/WEB-INF/jsp/Accueil.jsp").forward(request, response);
 	}
 
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
--- Encheres/src/fr/eni/javaee/encheres/ihm/gestionUtilisateur/ServletInscription.java
+++ Encheres/src/fr/eni/javaee/encheres/ihm/gestionUtilisateur/ServletInscription.java
@@ -7,13 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.eni.javaee.encheres.bll.utilisateur.UtilisateurManager;
+
 /**
  * Servlet implementation class ServletInscription
  */
 @WebServlet("/ServletInscription")
 public class ServletInscription extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -34,8 +36,26 @@ public class ServletInscription extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String pseudo = request.getParameter("pseudo");
+		String nom = request.getParameter("nom");
+		String prenom = request.getParameter("prenom");
+		String email = request.getParameter("email");
+		String telephone = request.getParameter("telephone");
+		String rue = request.getParameter("rue");
+		String cp = request.getParameter("cp");
+		String ville = request.getParameter("ville");
+		String mdp = request.getParameter("mdp");
+		String vmdp = request.getParameter("vmdp");
+
+		if(vmdp.equals(mdp)) {
+			
+		}
+		
+		UtilisateurManager um = new UtilisateurManager();
+		um.insertUtilisateur(pseudo, nom, prenom, email, telephone,
+				 rue, cp, ville, mdp);
+
+
 	}
 
 }
