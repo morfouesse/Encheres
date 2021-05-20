0ecde2aa27604080456eaa86efbee14a4dcb141b
morfouesse <antoine.morganmorfouesse@gmail.com>
2021-05-20 09:24:19 +0200

    utilisateurById

--- Encheres/src/fr/eni/javaee/encheres/dal/utilisateur/UtilisateurDao.java
+++ Encheres/src/fr/eni/javaee/encheres/dal/utilisateur/UtilisateurDao.java
@@ -5,23 +5,29 @@ import fr.eni.javaee.encheres.bo.Utilisateur;
 public interface UtilisateurDao {
 
 	Utilisateur insertUtilisateur(Utilisateur utilisateur);
-	
+
 	Utilisateur updateUtilisateur(int noUtilisateur, Utilisateur utilisateur);
-	
+
 	void deleteUtilisateur(int noUtilisateur);
-	
+
 	/**
 	 * @param pseudo
 	 * @param mdp
 	 * @return 0: utilisateur inexistant - 1: mdp invalide - 2:connexion reussie
 	 */
 	int connexionValide(String pseudo, String mdp);
-	
+
 	/**
 	 * @param pseudo
 	 * @param mdp
 	 * @return le noUtilisateur correspondant à la pair pseudo/mdp fournie en paramètres
 	 */
 	int selectNoUtilisateur(String pseudo, String mdp);
-	
+
+	/**
+	 * @param id utilisateur
+	 * @return un utilisateur
+	 */
+	Utilisateur selectById(int id);
+
 }
